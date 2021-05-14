5bc6f118272951fab80e1af619c15fbcdd156099
Lucas Santos <68041354+Lucassantoscoder@users.noreply.github.com>
2021-05-14 14:47:34 -0300

    Update Login_Automatizado_facebook.py

--- Login_Automatizado_facebook.py
+++ Login_Automatizado_facebook.py
@@ -44,11 +44,3 @@ btn_more.click()
 #pyautogui.click(280,74), (400,85)
 #pyautogui.typewrite("seu arquivo")
 #pyautogui.typewrite(['enter'])
-
-
-
-
-
-
-#driver.find_element_by_css_selector ('<div class="bp9cbjyn j83agx80 taijpn5t dfwzkoeu ni8dbmo4 stjgntxs"><span class="pq6dq46d kb5gq1qc pfnyh3mw oi9244e8"><i class="sp_ihSnwNsMqao sx_b4d710" role="img"></i></span><span class="d2edcug0 hpfvmrgz qv66sw1b c1et5uql rrkovp55 a8c37x1j keod5gw0 nxhoafnm aigsh9s9 d3f4x2em fe6kdd0r mau55g9w c8b282yb iv3no6db jq4qci2q a3bd9o3v lrazzd5p m9osqain" dir="auto"><span class="a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7 ltmttdrg g0qnabr5">Foto/vídeo</span></span></div>')
-#btn_more.click()
